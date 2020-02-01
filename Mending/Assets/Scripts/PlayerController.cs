b7a58527812662a6953b2c56217a9fadb036a123
LemonsOnFire <lemonsonfirepro@gmail.com>
2020-02-01 11:27:32 -0500

    It Fix NOW

---
+++ Mending/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //This is all about moving and jumping
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+
+        transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded==true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 6, ForceMode2D.Impulse);
+        }
+
+    }
+
+    //Below focuses on avoiding infinity jump
+
+    public bool isGrounded;
+
+    void OnCollisionStay2D(Collision2D Other)
+    {
+        if (Other.collider.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D Other)
+    {
+        if (Other.collider.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D Other)
+    {
+        if (Other.gameObject.tag == "PickUp")
+        {
+            Other.gameObject.SetActive(false);
+            Debug.Log("Test Dialogue Given");
+        }
+    }
+
+}

490a8554a3e52d73a2aa9f560201c7aa85f95fc4
kessenm <mallorykessen@gmail.com>
2020-02-02 00:22:31 -0500

    Patches appear in front of fabric now

--- Mending/Assets/Scripts/PatchMove.cs
+++ Mending/Assets/Scripts/PatchMove.cs
@@ -32,7 +32,7 @@ public class PatchMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_Space1 = new Vector2(-2.5f, 3.75f);
+        m_Space1 = new Vector2(-2.5f, 3.5f);
         m_Space2 = new Vector2(2.5f, 1.8f);
         m_Space3 = new Vector2(-2.5f, -.25f);
 
